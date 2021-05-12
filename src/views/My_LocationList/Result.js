744b2bcaf4391d15675e05520a5ae00e72798dec
seunwoo <leemoney93@naver.com>
2021-05-13 08:31:38 +0900

    바코드 추가

--- src/views/My_LocationList/Result.js
+++ src/views/My_LocationList/Result.js
@@ -8,7 +8,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import PropTypes from "prop-types";
-import Tooltip from "@material-ui/core/Tooltip";
+import Barcode from 'react-barcode';
 
 const useStyles = makeStyles({
   root: {
@@ -26,21 +26,23 @@ const useStyles = makeStyles({
 export default function Result({locations}) {
   const classes = useStyles();
 
+  console.log(locations)
+
   return (
     <TableContainer component={Paper} className={classes.root}>
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell align="center" className={classes.boldTableCell}>로케이션</TableCell>
+            {/*<TableCell align="center" className={classes.boldTableCell}>로케이션</TableCell>*/}
             <TableCell align="center" className={classes.boldTableCell}>바코드</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {locations.map((delivery) => (
-            <Fragment key={delivery.로케이션}>
+            <Fragment key={delivery.로케이션 + delivery.센터코드}>
               <TableRow className={classes.tableRows} hover>
-                <TableCell align="center">{delivery.로케이션}</TableCell>
-                <TableCell align="center">바코드예정</TableCell>
+                {/*<TableCell align="center">{delivery.로케이션}</TableCell>*/}
+                <TableCell align="center"><Barcode value={delivery.로케이션} /></TableCell>
               </TableRow>
             </Fragment>
           ))}
